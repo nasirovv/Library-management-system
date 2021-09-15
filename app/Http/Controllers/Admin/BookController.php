e4c91ff11de82d54a69896cdc6558c8dcf22320c
Mirfayz Nosirov <nasirov0017@gmail.com>
2021-09-15 16:47:53 +0300

    api for get books with advanced search finished

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -5,15 +5,44 @@ namespace App\Http\Controllers\Admin;
 use App\Facades\Image;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\BookRequest;
-use App\Http\Resources\Admin\BookResource;
 use App\Models\Book;
+use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $books = BookResource::collection(Book::all());
+        $text = strtolower($request->input('searchText'));
 
+        $books = Book::select('id', 'name', 'author', 'ISBN', 'description', 'originalCount', 'count', 'image', 'publishedYear', 'created_at')
+            ->with('categories:id,name')
+            ->when($request->input('filter') && $request->input('filter') === 'category', function ($query) use ($request, $text){
+                    return $query->whereHas('categories', function ($q) use ($text){
+                        return $q->where('name', 'LIKE', "%$text%");
+                    });
+            })
+            ->when($request->input('searchBy'), function ($query) use ($request, $text){
+                if ($request->input('searchBy') === 'author'){
+                    return $query->where('author', 'LIKE', "%$text%");
+                }
+                if ($request->input('searchBy') === 'ISBN'){
+                    return $query->where('ISBN', 'LIKE', "%$text%");
+                }
+                return $query->where('name', 'LIKE', "%$text%");
+            })
+            ->when($request->input('fromYear'), function ($query) use ($request){
+                return $query->where('publishedYear', '>=', $request->input('fromYear'));
+            })
+            ->when($request->input('toYear'), function ($query) use ($request){
+                return $query->where('publishedYear', '<=', $request->input('toYear'));
+            })
+            ->when($request->input('sort'), function ($query) use ($request){
+                if($request->input('sort') === 'alphabet'){
+                    return $query->orderBy('name');
+                }
+                return $query->orderByDesc('publishedYear');
+            })
+            ->simplePaginate(10);
         return response()->json($books, 200);
     }
 
@@ -32,7 +61,7 @@ class BookController extends Controller
         if($request->hasFile('image')){
             Image::upload($request->file('image'), 'books', $book->id);
         }
-
+        $book->categories()->attach(json_decode($request->categories));
         return response()->json('Successfully added', 201);
     }
 
@@ -60,7 +89,7 @@ class BookController extends Controller
         if ($request->hasFile('image')){
             Image::update($request->file('image'), 'books', $book->id);
         }
-
+        $book->categories()->sync(json_decode($request->categories));
         return response()->json('Successfully updated', 200);
     }
 
